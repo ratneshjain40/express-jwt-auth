e1f7b692fce342db26fb4f85d8ccca0bcbcc6868
Ratnesh <ratneshjain40@gmail.com>
2022-09-08 22:17:17 +0530

    added verification of jwt

--- auth/utils.js
+++ auth/utils.js
@@ -41,6 +41,30 @@ function issueJWT(user) {
   }
 }
 
+function verifyJWT(token) {
+	var payload
+	try {
+		// Parse the JWT string and store the result in `payload`.
+		// Note that we are passing the key in this method as well. This method will throw an error
+		// if the token is invalid (if it has expired according to the expiry time we set on sign in),
+		// or if the signature does not match
+    // mention the algorithm in the verify method to use the same algorithm as the one used to sign the token
+		payload = jsonwebtoken.verify(token, PUB_KEY, { algorithms: ['RS256'] })
+	} catch (e) {
+		if (e instanceof jwt.JsonWebTokenError) {
+			// if the error thrown is because the JWT is unauthorized, return a 401 error
+			return {
+        status: 401,
+        message: 'unauthorized'
+      }
+		}
+		// otherwise, return a bad request error
+		return {
+      status: 400,
+      message: 'bad request'
+    }
+	}
+}
 module.exports.validPassword = validPassword;
 module.exports.genPassword = genPassword;
 module.exports.issueJWT = issueJWT;
